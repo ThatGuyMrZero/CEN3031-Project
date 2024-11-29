5aa98cdd1c203e7046e600b0fd265a7f8c06c35e
sschlingmann04 <sschlingmann@ufl.edu>
2024-11-29 17:19:13 -0500

    removed unnecessary comments

--- login.js
+++ login.js
@@ -1,7 +1,3 @@
-
-
-// Test
-
 let login_form = document.getElementById("login");
 login_form.addEventListener('submit', Login);
 
@@ -11,14 +7,9 @@ function Login(event) {
     const button = document.querySelector("#login_submit");
     const login_data = new FormData(event.target, button);
 
-    //login_data.keys().forEach( (x) => {console.log(x)} );
-    //console.log(login_data['user']);
-    //console.log(login_data.get('user'));
-
     let new_div = document.body.createElement('div');
     new_div.style = "border: 2px black dotted";
     document.body.append(new_div);
     new_div.innerText = login_data.get('user');
 
-    //document.body.innerHTML += login_data.get('user');
 }
